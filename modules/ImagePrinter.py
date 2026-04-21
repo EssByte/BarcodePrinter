16a116e4fa4a6a29dcf9192ec98373defe682783
thelonewolf <thelonewolf06@proton.me>
2026-04-21 20:21:53 +0800

    Style: further reduced description font size to 40 for a compact layout

--- modules/ImagePrinter.py
+++ modules/ImagePrinter.py
@@ -44,7 +44,7 @@ class ImagePrinter:
             font_small = ImageFont.truetype(font_path, 20)
             font_medium = ImageFont.truetype(font_path, 30)
             font_large = ImageFont.truetype(font_path, 40)
-            font_huge = ImageFont.truetype(font_path, 60)
+            font_huge = ImageFont.truetype(font_path, 40)
         except:
             # Final fallback to built-in tiny font if all files fail
             font_small = font_medium = font_large = font_huge = ImageFont.load_default()
