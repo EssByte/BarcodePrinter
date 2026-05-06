d02340baa43aab5c2e8b2fac9c98f5f2641c1425
Eswaran <eswaran@hermes>
2026-05-06 14:54:40 +0200

    Move remark to right side of 35x25 label

--- modules/ImagePrinter.py
+++ modules/ImagePrinter.py
@@ -209,10 +209,10 @@ class ImagePrinter:
             draw.text((BASE_X, curr_y), line, fill=0, font=f_name)
             curr_y += 15
 
-        # 4b. Remark (below description)
+        # 4b. Remark (below description, right side)
         remark = data.get('remark', '')
         if remark:
-            draw.text((BASE_X, curr_y), remark, fill=0, font=f_name)
+            draw.text((350, curr_y), remark, fill=0, font=f_name)
             curr_y += 15
 
         # 5. Price
