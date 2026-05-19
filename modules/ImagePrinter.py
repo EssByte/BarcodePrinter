016f4a48c6ed0a2ccd390ea10a398f49d149900d
thelonewolf <eseswaran46@proton.me>
2026-05-19 21:58:27 +0800

    Fix label drift by adding GAP command to TSPL header

--- modules/ImagePrinter.py
+++ modules/ImagePrinter.py
@@ -340,6 +340,7 @@ class ImagePrinter:
             "DENSITY 7\r\n"
             "DIRECTION 0\r\n"
             f"SIZE {width_mm}MM, {height_mm}MM\r\n"
+            "GAP 2MM, 0MM\r\n"
             "REFERENCE 0,0\r\n"
             "CLS\r\n"
         ).encode('utf-8')
