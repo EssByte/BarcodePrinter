aa7eafbbf62425757fd8882c1d6d1a47b5d9a61b
thelonewolf <eseswaran46@proton.me>
2026-05-19 22:27:29 +0800

    Revert TSPL commands entirely to original state

--- modules/ImagePrinter.py
+++ modules/ImagePrinter.py
@@ -339,8 +339,7 @@ class ImagePrinter:
             "SPEED 2.0\r\n"
             "DENSITY 7\r\n"
             "DIRECTION 0\r\n"
-            f"SIZE {width_mm} mm, {height_mm} mm\r\n"
-            "GAP 2 mm, 0 mm\r\n"
+            f"SIZE {width_mm}MM, {height_mm}MM\r\n"
             "REFERENCE 0,0\r\n"
             "CLS\r\n"
         ).encode('utf-8')
